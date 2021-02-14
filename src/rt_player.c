0cfc6f45e26c5004389b35f1e382415e979833cb
Pierre Foucart <pierre.foucart@epita.fr>
2021-02-14 15:36:00 +0100

    rt_player: Update main to get user input
    There is an awkward newline problem, but it is not really this
    important.

--- src/rt_player.c
+++ src/rt_player.c
@@ -4,15 +4,28 @@
 #include "notes.h"
 #include "rt_note_player.h"
 
+#define LAST_NOTE_IDX NOTES_RANGE - 1
+
+// Get a note index from user input
+int get_note_idx(void)
+{
+    char in = getchar();
+
+    if (in < 'a' || in > 'a' + LAST_NOTE_IDX)
+        return NOTHING;
+
+    return in - 'a';
+}
+
 int main(void)
 {
     audio_setup();
 
     while (1)
     {
-        for (int node_idx = 0; node_idx < NOTES_RANGE; node_idx++)
+        for (int node_idx = 0; node_idx < NOTES_RANGE - 1; node_idx++)
         {
-            printf("Playing note %d\n", node_idx);
+            int node_idx = get_note_idx();
             set_note(node_idx);
             sleep(1);
         }
